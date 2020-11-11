befd58a4034901aeba1ed58c7333698650a7c0bd
ilshat <ilshat.khairakhun@gmail.com>
2020-11-11 17:18:41 +0600

    Initial Commit

---
+++ GotChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GotChat
+//
+//  Created by Ilshat Khairakhun on 11/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
